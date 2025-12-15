72ea17cdff156b1fc11b33840e5b62886eb42948
gorhill <gorhill@users.noreply.github.com>
2025-12-15 14:56:37 +0000

    [workflow] Update uBOLite MV3 package files for 2025.1215.1455
    Files changed:
    M	chromium/log.txt
    M	chromium/manifest.json
    M	chromium/rulesets/main/adguard-spyware-url.json
    M	chromium/rulesets/main/annoyances-others.json
    M	chromium/rulesets/main/chn-0.json
    M	chromium/rulesets/main/dpollock-0.json
    M	chromium/rulesets/main/easylist.json
    M	chromium/rulesets/main/easyprivacy.json
    M	chromium/rulesets/main/fra-0.json
    M	chromium/rulesets/main/jpn-1.json
    M	chromium/rulesets/main/pgl.json
    M	chromium/rulesets/main/spa-1.json
    M	chromium/rulesets/main/stevenblack-hosts.json
    M	chromium/rulesets/main/swe-1.json
    M	chromium/rulesets/main/ublock-badware.json
    M	chromium/rulesets/main/ublock-filters.json
    M	chromium/rulesets/main/ukr-0.json
    M	chromium/rulesets/main/urlhaus-full.json
    M	chromium/rulesets/main/vie-1.json
    M	chromium/rulesets/ruleset-details.json
    M	chromium/rulesets/scripting/generic/chn-0.js
    M	chromium/rulesets/scripting/generic/jpn-1.js
    M	chromium/rulesets/scripting/generic/spa-1.js
    M	chromium/rulesets/scripting/generichigh/fra-0.css
    M	chromium/rulesets/scripting/procedural/annoyances-overlays.json
    M	chromium/rulesets/scripting/procedural/easylist.json
    M	chromium/rulesets/scripting/procedural/jpn-1.json
    M	chromium/rulesets/scripting/procedural/rus-0.json
    M	chromium/rulesets/scripting/procedural/spa-1.json
    M	chromium/rulesets/scripting/scriptlet/isolated/annoyances-cookies.js
    M	chromium/rulesets/scripting/scriptlet/isolated/ublock-filters.js
    M	chromium/rulesets/scripting/scriptlet/main/adguard-mobile.js
    M	chromium/rulesets/scripting/scriptlet/main/annoyances-overlays.js
    M	chromium/rulesets/scripting/scriptlet/main/chn-0.js
    M	chromium/rulesets/scripting/scriptlet/main/easyprivacy.js
    M	chromium/rulesets/scripting/scriptlet/main/fin-0.js
    M	chromium/rulesets/scripting/scriptlet/main/fra-0.js
    M	chromium/rulesets/scripting/scriptlet/main/idn-0.js
    M	chromium/rulesets/scripting/scriptlet/main/irn-0.js
    M	chromium/rulesets/scripting/scriptlet/main/isr-0.js
    M	chromium/rulesets/scripting/scriptlet/main/jpn-1.js
    M	chromium/rulesets/scripting/scriptlet/main/kor-1.js
    M	chromium/rulesets/scripting/scriptlet/main/nld-0.js
    M	chromium/rulesets/scripting/scriptlet/main/nor-0.js
    M	chromium/rulesets/scripting/scriptlet/main/rus-0.js
    M	chromium/rulesets/scripting/scriptlet/main/rus-1.js
    M	chromium/rulesets/scripting/scriptlet/main/spa-1.js
    M	chromium/rulesets/scripting/scriptlet/main/swe-1.js
    M	chromium/rulesets/scripting/scriptlet/main/tur-0.js
    M	chromium/rulesets/scripting/scriptlet/main/ublock-filters.js
    M	chromium/rulesets/scripting/scriptlet/main/ubol-tests.js
    M	chromium/rulesets/scripting/scriptlet/main/ukr-0.js
    M	chromium/rulesets/scripting/scriptlet/main/vie-1.js
    M	chromium/rulesets/scripting/specific/adguard-mobile.json
    M	chromium/rulesets/scripting/specific/annoyances-cookies.json
    M	chromium/rulesets/scripting/specific/annoyances-others.json
    M	chromium/rulesets/scripting/specific/easylist.json
    M	chromium/rulesets/scripting/specific/fra-0.json
    M	chromium/rulesets/scripting/specific/hun-0.json
    M	chromium/rulesets/scripting/specific/jpn-1.json
    M	chromium/rulesets/scripting/specific/rus-0.json
    M	chromium/rulesets/scripting/specific/spa-1.json
    M	chromium/rulesets/scripting/specific/ublock-filters.json
    M	chromium/rulesets/scripting/specific/vie-1.json
    M	chromium/rulesets/scriptlet-details.json
    M	chromium/rulesets/strictblock/chn-0.json
    M	chromium/rulesets/strictblock/dpollock-0.json
    M	chromium/rulesets/strictblock/easylist.json
    M	chromium/rulesets/strictblock/easyprivacy.json
    M	chromium/rulesets/strictblock/fra-0.json
    M	chromium/rulesets/strictblock/pgl.json
    M	chromium/rulesets/strictblock/stevenblack-hosts.json
    M	chromium/rulesets/strictblock/ublock-badware.json
    M	chromium/rulesets/strictblock/ublock-filters.json
    M	chromium/rulesets/strictblock/ukr-0.json
    M	chromium/rulesets/strictblock/urlhaus-full.json

--- chromium/rulesets/scripting/scriptlet/main/ukr-0.js
+++ chromium/rulesets/scripting/scriptlet/main/ukr-0.js
@@ -220,7 +220,7 @@ function collateFetchArgumentsFn(resource, options) {
     const props = [
         'body', 'cache', 'credentials', 'duplex', 'headers',
         'integrity', 'keepalive', 'method', 'mode', 'priority',
-        'redirect', 'referrer', 'referrerPolicy', 'signal',
+        'redirect', 'referrer', 'referrerPolicy', 'signal', 'url'
     ];
     const out = {};
     if ( collateFetchArgumentsFn.collateKnownProps === undefined ) {
